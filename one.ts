18d5471c2849fee89de29a1601e16cbe8b74bf44
Shahriar Chowdhury <117475029+hcshahriar@users.noreply.github.com>
2025-06-23 04:06:30 +0600

    Create one.ts

---
+++ one.ts
@@ -0,0 +1,9 @@
+let message: string = "Hello, TypeScript!";
+let age: number = 25;
+let isActive: boolean = true;
+
+
+let numbers: number[] = [1, 2, 3];
+let names: Array<string> = ["Alice", "Bob"];
+
+let person: [string, number] = ["Alice", 30];
