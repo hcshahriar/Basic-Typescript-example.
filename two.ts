193b2d6d4f29c063bcadd03ac95a7479cef8a075
Shahriar Chowdhury <117475029+hcshahriar@users.noreply.github.com>
2025-06-23 04:07:45 +0600

    Create two.ts

---
+++ two.ts
@@ -0,0 +1,10 @@
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+const greet = (name: string): string => {
+  return `Hello, ${name}!`;
+};
+
+console.log(add(5, 3)); 
+console.log(greet("TypeScript")); 
